98d85f699f397687e171fa71fb5f70eb87b8fd1c
CarlosAruh <carlos.aruh@gmail.com>
2023-07-25 09:38:28 -0300

    Corrigindo @babel

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
